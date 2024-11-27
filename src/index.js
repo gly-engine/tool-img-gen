981b4b804aee09662fc6febb0690f68d463b8cc3
RodrigoDornelles <rodrigo@dornelles.me>
2024-11-27 10:43:07 -0300

    feat: revision 0.0.15 (#7)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import wasmFile from 'wasmoon/dist/glue.wasm';
 import { LuaFactory, LuaMultiReturn } from 'wasmoon'
 import { editor } from 'monaco-editor'
 import gly from '@gamely/core-native-html5'
-import gly_engine from '@gamely/gly-engine/dist/main.lua'
+import gly_engine from '@gamely/gly-engine-lite/dist/main.lua'
 import defaultScript from './default.lua'
 
 let monacoTimeout;
@@ -30,11 +30,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     const lua = await factory.createEngine()
     await lua.doString(gly_engine)
 
-    gly.global.set('native_callback_init', lua.global.get('native_callback_init'))
-    gly.global.set('native_callback_loop', lua.global.get('native_callback_loop'))
-    gly.global.set('native_callback_draw', lua.global.get('native_callback_draw'))
-    gly.global.set('native_callback_resize', lua.global.get('native_callback_resize'))
-    gly.global.set('native_callback_keyboard', lua.global.get('native_callback_keyboard'))
     lua.global.set('native_draw_start', gly.global.get('native_draw_start'))
     lua.global.set('native_draw_flush', gly.global.get('native_draw_flush'))
     lua.global.set('native_draw_clear', gly.global.get('native_draw_clear'))
@@ -46,19 +41,30 @@ document.addEventListener('DOMContentLoaded', async () => {
     lua.global.set('native_dict_http', gly.global.get('native_dict_http'))
     lua.global.set('native_dict_json', gly.global.get('native_dict_json'))
     lua.global.set('native_dict_poly', gly.global.get('native_dict_poly'))
-    lua.global.set('native_draw_text', (x, y, text) => {
-        const native_draw_text = gly.global.get('native_draw_text')
+    lua.global.set('native_text_print', gly.global.get('native_text_print'))
+    lua.global.set('native_text_font_size', gly.global.get('native_text_font_size'))
+    lua.global.set('native_text_font_name', gly.global.get('native_text_font_name'))
+    lua.global.set('native_text_font_default', gly.global.get('native_text_font_default'))
+    lua.global.set('native_text_font_previous', gly.global.get('native_text_font_previous'))
+    lua.global.set('native_text_mensure', (x, y, text) => {
+        const native_draw_text = gly.global.get('native_text_mensure')
         return LuaMultiReturn.from(native_draw_text(x, y, text))
     })
 
+    await lua.doString(gly_engine)
+
+    gly.global.set('native_callback_init', lua.global.get('native_callback_init'))
+    gly.global.set('native_callback_loop', lua.global.get('native_callback_loop'))
+    gly.global.set('native_callback_draw', lua.global.get('native_callback_draw'))
+    gly.global.set('native_callback_resize', lua.global.get('native_callback_resize'))
+
     gly.error('canvas')
-    gly.init('#gameCanvas')
+    gly.init(elCanvas)
 
     const apply = () => {
         const code = monacoEditor.getValue()
-        gly.load(`return {draw=function(std)\nprint('oi')\n${code}\nend}`)
-        elCanvas.width = elInpWidth.value;
-        elCanvas.height= elInpHeight.value;
+        gly.load(`return {init=function()end,loop=function()end,draw=function(std)\n${code}\nend}`)
+        gly.resize(elInpWidth.value, elInpHeight.value)
         window.requestAnimationFrame(gly.update)
     }
 
